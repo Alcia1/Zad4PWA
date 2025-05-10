9df51ee21e7e3afcafa5e9d9b52c59a3fd453401
Alcia1 <63465009+Alcia1@users.noreply.github.com>
2025-05-10 14:10:49 +0200

    host

--- sw.js
+++ sw.js
@@ -1,47 +1,52 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('my-cache').then(function(cache) {
-        const filesToCache = [
-          '/',
-          '/index.html',
-          '/shopping.html',
-          '/weather.html',
-          '/scripts/script.js',
-          '/scripts/script_weather.js',
-          '/css/style.css',
-          '/img/favicon.png',
-          '/img/sun.png',
-          '/img/icon192.png',
-          '/img/icon512.png'
-        ];
-  
-        return Promise.all(
-          filesToCache.map(url =>
-            fetch(url)
-              .then(response => {
-                if (!response.ok) throw new Error(`HTTP error! ${response.status}`);
-                return cache.put(url, response.clone());
-              })
-          )
-        );
-      })
-    );
-    self.skipWaiting();
-  });
-  
+// sw.js
 
-  
-  self.addEventListener('activate', function(event) {
-    event.waitUntil(clients.claim()); // przejmuje kontrolę nad otwartymi zakładkami
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          return response || fetch(event.request);
+
+
+self.addEventListener('install', function(event) {
+
+    event.waitUntil(
+
+        caches.open('my-cache').then(function(cache) {
+
+            return cache.addAll([
+
+                '/index.html',
+
+                '/shopping.html',
+
+                '/weather.html',
+
+                '/scripts/script.js',
+
+                '/scripts/script_weather.js',
+
+                '/css/style.css',
+                
+                '/img/favicon.png',
+
+                '/img/sun.png',
+
+                '/img/icon192.png',
+
+                '/img/icon512.png'
+            ]);
+
         })
-        .catch(() => caches.match('/index.html')) // fallback na offline
+
     );
-  });
-  
+
+});
+
+self.addEventListener('fetch', function(event) {
+
+    event.respondWith(
+
+        caches.match(event.request).then(function(response) {
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
+});
