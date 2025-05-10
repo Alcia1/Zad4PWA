fbde55def84d18d43258843e75360798b8011747
Alcia1 <63465009+Alcia1@users.noreply.github.com>
2025-05-10 13:42:27 +0200

    host

--- sw.js
+++ sw.js
@@ -45,16 +45,26 @@ self.addEventListener('install', function(event) {
 
 });
 
+// self.addEventListener('fetch', function(event) {
+
+//     event.respondWith(
+
+//         caches.match(event.request).then(function(response) {
+
+//             return response || fetch(event.request);
+
+//         })
+
+//     );
+
+// });
+
 self.addEventListener('fetch', function(event) {
-
     event.respondWith(
-
-        caches.match(event.request).then(function(response) {
-
-            return response || fetch(event.request);
-
+      caches.match(event.request)
+        .then(function(response) {
+          return response || fetch(event.request);
         })
-
+        .catch(() => caches.match('/index.html')) // fallback offline
     );
-
-});
+  });
